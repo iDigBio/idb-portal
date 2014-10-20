aab8c29bdc0c6d4238ec82d5d60806eecf426fd7
Greg Traub <greg.traub@gmail.com>
2014-10-20 16:37:03 -0400

    more filter interactivity work

--- public/client/js/react/build/search/filters.js
+++ public/client/js/react/build/search/filters.js
@@ -7,11 +7,11 @@ var fields = require('../../../lib/fields');
 
 module.exports = React.createClass({displayName: 'exports',
     getInitialState: function(){
-        return {filters: []};
+        return {filters: [], filterType: 'text'};
     },
     addFilter: function(event){
         var cur = this.state.filters;
-        cur.unshift(event.currentTarget.value);
+        cur.unshift({name: event.currentTarget.value, type: this.state.filterType});
         this.setState({filters: cur});
     },
     removeFilter: function(event){
@@ -19,19 +19,66 @@ module.exports = React.createClass({displayName: 'exports',
         cur.splice(cur.indexOf(event.currentTarget.attributes['data-remove'].value),1);
         this.setState({filters: cur});
     },
+    changeFilterType: function(event){
+        this.setState({filterType: event.currentTarget.value});
+    },
+    makeFilter: function(fltrObj){
+        var type = fltrObj.type, name = fltrObj.name;
+        switch(type){
+            case 'text':
+                return(
+                    React.DOM.div({className: "option-group filter"}, 
+                        React.DOM.i({className: "glyphicon glyphicon-remove", onClick: this.removeFilter, 'data-remove': name}), 
+                        React.DOM.label({className: "filter-name"}, name), 
+                        React.DOM.textarea({className: "form-control", placeholder: fields.byName[name].dataterm}
+                        )
+                    )
+                );
+            case 'presence':
+                return(
+                    React.DOM.div({className: "option-group filter"}, 
+                        React.DOM.i({className: "glyphicon glyphicon-remove", onClick: this.removeFilter, 'data-remove': name}), 
+                        React.DOM.label({className: "filter-name"}, name), 
+                        
+                            React.DOM.div({className: "checkbox"}, 
+                                React.DOM.label(null, 
+                                    React.DOM.input({type: "checkbox"}), 
+                                    "Present"
+                                )
+                            ), 
+                            React.DOM.div({className: "checkbox"}, 
+                                React.DOM.label(null, 
+                                    React.DOM.input({type: "checkbox"}), 
+                                    "Missing"
+                                )
+                            )
+                        
+                    )
+                );       
+        }
+    },
+    filters: function(){
+        var list = [];
+        _.each(this.state.filters,function(item){
+            list.push(item.name);
+        });
+        return list;
+    },
     render: function(){
-        var fgroups =[], self=this;
+        var self=this;
+        var ft = self.state.filterType;
+        var fgroups =[React.DOM.option({value: "0", key: "0"}, "select to add ", ft, " filter")];
         var groups = ['taxonomy','specimen','collectionevent','locality'];
+        var filterlist = this.filters();
         _.each(groups,function(val){
             var flist = [];
             _.each(fields.byGroup[val],function(field){
                 if(field.hidden===1){
                     //noop
                 }else{
-                    var disabled = self.state.filters.indexOf(field.name) === -1 ? '' : 'disabled';
-                    var name = field.name;
+                    var disabled = filterlist.indexOf(field.name) === -1 ? '' : 'disabled';
                     flist.push(
-                            React.DOM.option({disabled: disabled, value: field.name}, 
+                            React.DOM.option({disabled: disabled, value: field.name, key: field.name}, 
                                 field.name
                             )
                     );
@@ -51,25 +98,30 @@ module.exports = React.createClass({displayName: 'exports',
         var filters = [];
         _.each(this.state.filters,function(item){
             filters.push(
-                React.DOM.div({className: "option-group filter"}, 
-                    React.DOM.i({className: "glyphicon glyphicon-remove", onClick: self.removeFilter, 'data-remove': item}), 
-                    React.DOM.label({className: "filter-name"}, item), 
-                    React.DOM.textarea({className: "form-control", placeholder: fields.byName[item].dataterm}
-                    )
-
-                )
+                self.makeFilter(item)
             )
         })
         return (
             React.DOM.div(null, 
                 React.DOM.div({className: "option-group", id: "filter-select"}, 
-                    React.DOM.ul({id: "filter-type"}, 
-                        React.DOM.li({className: "active"}, "Text Filter"), 
-                        React.DOM.li(null, "Presence Filter")
+
+                    React.DOM.div({className: "clearfix", id: "filter-type"}, 
+                    React.DOM.img({src: "/portal/img/type.svg"}), 
+                       React.DOM.div({className: "filter-type"}, 
+                            React.DOM.label(null, 
+                                React.DOM.input({onChange: this.changeFilterType, type: "radio", name: "filter-type", value: "text"}), 
+                                "Text Filter"
+                            )
+                        ), 
+                        React.DOM.div({className: "filter-type"}, 
+                            React.DOM.label(null, 
+                                React.DOM.input({onChange: this.changeFilterType, type: "radio", name: "filter-type", value: "presence"}), 
+                                "Presence Filter"
+                            )
+                       )
                     ), 
                     React.DOM.div({id: "filter-selects", className: "clearfix"}, 
                         React.DOM.select({className: "form-control", value: "0", placeholder: "select to add", onChange: this.addFilter}, 
-                            React.DOM.option({value: "0"}, "select to add"), 
                             fgroups
                         )
                     )
--- public/client/js/react/src/search/filters.js
+++ public/client/js/react/src/search/filters.js
@@ -7,11 +7,11 @@ var fields = require('../../../lib/fields');
 
 module.exports = React.createClass({
     getInitialState: function(){
-        return {filters: []};
+        return {filters: [], filterType: 'text'};
     },
     addFilter: function(event){
         var cur = this.state.filters;
-        cur.unshift(event.currentTarget.value);
+        cur.unshift({name: event.currentTarget.value, type: this.state.filterType});
         this.setState({filters: cur});
     },
     removeFilter: function(event){
@@ -19,19 +19,66 @@ module.exports = React.createClass({
         cur.splice(cur.indexOf(event.currentTarget.attributes['data-remove'].value),1);
         this.setState({filters: cur});
     },
+    changeFilterType: function(event){
+        this.setState({filterType: event.currentTarget.value});
+    },
+    makeFilter: function(fltrObj){
+        var type = fltrObj.type, name = fltrObj.name;
+        switch(type){
+            case 'text':
+                return(
+                    <div className="option-group filter">
+                        <i className="glyphicon glyphicon-remove" onClick={this.removeFilter} data-remove={name}></i>
+                        <label className="filter-name">{name}</label>
+                        <textarea className="form-control" placeholder={fields.byName[name].dataterm}>
+                        </textarea>
+                    </div>
+                );
+            case 'presence':
+                return(
+                    <div className="option-group filter">
+                        <i className="glyphicon glyphicon-remove" onClick={this.removeFilter} data-remove={name}></i>
+                        <label className="filter-name">{name}</label>
+                        
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" />
+                                    Present
+                                </label>
+                            </div>
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" />
+                                    Missing
+                                </label>
+                            </div>
+                        
+                    </div>
+                );       
+        }
+    },
+    filters: function(){
+        var list = [];
+        _.each(this.state.filters,function(item){
+            list.push(item.name);
+        });
+        return list;
+    },
     render: function(){
-        var fgroups =[], self=this;
+        var self=this;
+        var ft = self.state.filterType;
+        var fgroups =[<option value="0" key="0">select to add {ft} filter</option>];
         var groups = ['taxonomy','specimen','collectionevent','locality'];
+        var filterlist = this.filters();
         _.each(groups,function(val){
             var flist = [];
             _.each(fields.byGroup[val],function(field){
                 if(field.hidden===1){
                     //noop
                 }else{
-                    var disabled = self.state.filters.indexOf(field.name) === -1 ? '' : 'disabled';
-                    var name = field.name;
+                    var disabled = filterlist.indexOf(field.name) === -1 ? '' : 'disabled';
                     flist.push(
-                            <option disabled={disabled} value={field.name}>
+                            <option disabled={disabled} value={field.name} key={field.name}>
                                 {field.name}
                             </option>
                     );
@@ -51,25 +98,30 @@ module.exports = React.createClass({
         var filters = [];
         _.each(this.state.filters,function(item){
             filters.push(
-                <div className="option-group filter">
-                    <i className="glyphicon glyphicon-remove" onClick={self.removeFilter} data-remove={item}></i>
-                    <label className="filter-name">{item}</label>
-                    <textarea className="form-control" placeholder={fields.byName[item].dataterm}>
-                    </textarea>
-
-                </div>
+                self.makeFilter(item)
             )
         })
         return (
             <div>
                 <div className="option-group" id="filter-select">
-                    <ul id="filter-type">
-                        <li className="active">Text Filter</li>
-                        <li>Presence Filter</li>
-                    </ul>
+
+                    <div className="clearfix" id="filter-type">
+                    <img src="/portal/img/type.svg"/>
+                       <div className="filter-type">
+                            <label >
+                                <input onChange={this.changeFilterType} type="radio" name="filter-type" value="text"/>
+                                Text Filter
+                            </label>
+                        </div>
+                        <div className="filter-type">
+                            <label>
+                                <input onChange={this.changeFilterType} type="radio" name="filter-type" value="presence"/>
+                                Presence Filter
+                            </label>
+                       </div>
+                    </div>
                     <div id="filter-selects" className="clearfix">
                         <select className="form-control" value="0" placeholder="select to add" onChange={this.addFilter}>
-                            <option value="0">select to add</option>
                             {fgroups}
                         </select>
                     </div>

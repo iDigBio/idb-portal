f12360b0bea359c7fd52eab897848872382f72b9
Greg Traub <gtraub@Gregs-MacBook-Pro.local>
2015-01-22 11:28:57 -0500

    more map

--- public/client/js/lib/mapper.js
+++ public/client/js/lib/mapper.js
@@ -1,6 +1,7 @@
 
 var L = require('leaflet/dist/leaflet');
 var $ = require('jquery');
+var _ = require('lodash');
 //elid: string name of element id;
 //options: object map of settings
 /*
@@ -26,10 +27,12 @@ module.exports = IDBMap =  function(elid, options){
     if(typeof options == 'object'){
         _.merge(this.defaults,options);
     }
-    
+    //init map
     this.map = L.map(elid,this.defaults);
     this.map.addControl(new ExpandButton());
-
+    //add modal pane for expanded view
+    $('body').append('<div id="mapper-modal"></div>');
+    //
     this.currentQueryTime = 0;
     var idblayer;
     
@@ -69,7 +72,28 @@ var ExpandButton =  L.Control.extend({
     onAdd: function(map){
         var selfish = this;
         this._div.innerHTML = '<div title="maximize map" id="map-expand-button" class="map-button-icon"></div>';
+        var cont = map.getContainer();
+        L.DomEvent.addListener(this._div, 'click', function (e) {
+            L.DomEvent.stopPropagation(e);
+            var width = $(window).width(), height = $(window).height();
+            var pos = $(cont).position();
+            $(cont).css('position', 'fixed').css('top',pos.top+'px').css('left',pos.left+'px').css('z-index','550');
+            $(cont).animate({width: (width-53), height: (height-53), margin: 25, left:0,top:0},{
+                duration: 300,
+                complete: function(){
+                    $('#mapper-modal').show();
+                    map.invalidateSize();
+                },
+                progress: function(){
+                    map.invalidateSize();
+                }
+            });
+            map.removeControl(selfish);
+        });
         return this._div;
+    },
+    onRemove: function(map){
+        
     }
 });
 

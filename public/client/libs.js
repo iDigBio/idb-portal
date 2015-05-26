51b84652757a12b10e65302717dac1646864432b
Greg Traub <greg.traub@gmail.com>
2015-05-26 11:07:04 -0400

    update libs file with tablesorter

--- public/client/libs.js
+++ public/client/libs.js
@@ -13,7 +13,7 @@ var SearchHistory = require('./js/lib/history');
 window.searchHistory = new SearchHistory;
 //window.React = require('react');
 require('../components/bootstrap/dist/js/bootstrap.min');
-
+require('../components/jquery.tablesorter/js/jquery.tablesorter.min');
 //$.fn.datetimepicker = require('eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker');
 require('es5-shim/es5-shim.min');
 require('es5-shim/es5-sham.min');

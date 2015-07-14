7ff30c3eede9ca6cd575d5c20a96c4a900370a2e
Greg Traub <greg.traub@gmail.com>
2015-07-14 12:15:07 -0400

    another header change

--- config/express.js
+++ config/express.js
@@ -68,10 +68,10 @@ module.exports = function(app, config) {
                 req.user = {};
             }
             //is user on portal.idigbio.org or www.idigbio.org/portal ?
-            if(req.headers.host.indexOf('portal') === 0){
-                req.user.refurl = req.originalUrl;
-            }else{
+            if(_.has(req.headers,'x-forwarded-host') && req.headers['x-forwarded-host'] == 'www.idigbio.org'){
                 req.user.refurl = '/portal'+req.originalUrl;
+            }else{
+                req.user.refurl = req.originalUrl;
             }
             
             req.user.host = req.headers.host;

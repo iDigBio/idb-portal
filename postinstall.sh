dbc498020a3ff0c700e7d55572bf323fbc507b94
Greg Traub <greg.traub@gmail.com>
2015-08-05 10:49:52 -0400

    cleaned up postinstall files

--- postinstall.sh
+++ postinstall.sh
@@ -5,19 +5,11 @@ echo "+++++++++++++ BEGIN POSTINSTALL +++++++++++++"
 # Client Side Dependancies
 node_modules/bower/bin/bower install
 
-# Build Leaflet
-#cd public/components/leaflet
-#npm install
-#cd ../../..
-
-# Build Knockout
-#cd public/components/knockout/
-#npm install
-#cd ../../..
+#compile jsx to build files for server side use of React
 babel public/client/js/react/src/ --out-dir public/client/js/react/build/ --blacklist "strict"
-#node_modules/browserify/bin/cmd.js -o public/js/client.js  public/client/js/main.js -t babelify
+#compile full client side file with jsx transforms for browser side client
 node_modules/browserify/bin/cmd.js -o public/js/client.js  public/client/js/main.js -t [ babelify --blacklist "strict" ]
-#
+#minify client side file
 node_modules/uglify-js/bin/uglifyjs -o public/js/client.js public/js/client.js
 
 #node_modules/uglify-js/bin/uglifyjs -o public/components/underscore/underscore-min.js public/components/underscore/underscore.js

435aa5d46e71fcb62c3dcac4b90a7cf868933f46
Richard Garand <rjgarand@gmail.com>
2016-05-26 16:38:28 -0400

    initializeTouchEvents baked into React 15 by default

--- public/client/js/search.js
+++ public/client/js/search.js
@@ -1,9 +1,6 @@
-
-
 var React = require('react')
 var SearchPage = require('./react/src/search');
 
-React.initializeTouchEvents(true);
 React.render(
     <SearchPage />,
     document.getElementById('main')

cd6005b02e32061dceaef35d087dd26850ac9cab
Alex Thompson <godfoder@acis.ufl.edu>
2017-12-01 16:20:39 -0500

    css import migration.

--- public/client/js/search.js
+++ public/client/js/search.js
@@ -2,6 +2,10 @@ var React = require('react')
 var ReactDom = require('react-dom');
 var SearchPage = require('./react/src/search');
 
+import '@bower_components/leaflet-loading/src/Control.Loading.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
+import '@bower_components/leaflet.fullscreen/Control.FullScreen.css';
+
 ReactDom.render(
     <SearchPage />,
     document.getElementById('main')

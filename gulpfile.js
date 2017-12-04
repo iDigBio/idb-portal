4ca1b8de13556ac3a9b8aaaaf03efdbd0854b1e4
Alex Thompson <godfoder@acis.ufl.edu>
2017-12-04 14:05:48 -0500

    work new gulp build with asset inlining.

--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,124 @@
-/*
-*Default task requires Chrome LiveReload plugin for livereload development.
-*otherwise this can be run just for browserify bundleing.
-*/
-var gulp = require('gulp'),
-    livereload = require('gulp-livereload'),
-    source = require('vinyl-source-stream'),
-    gutil = require('gulp-util'),
-    watchify = require('watchify'),
-    gulpbrowserify = require('gulp-browserify'),
-    browserify = require('browserify'),
-    less = require('gulp-less'),
-    uglify = require('gulp-uglify'),
-    path = require('path'),
-    rename = require('gulp-rename'),
-    babel = require('gulp-babel'),
-    babelify = require('babelify'),
-    buffer = require('vinyl-buffer'),
-    browserifyCss = require('browserify-css');
+'use strict';
 
-/*
-Task: default
-runs Live Reload server for doing active development. Watches files for updates and 
-builds all react and less files and client files that are part of standard development cycle.
-see  /public/client/js /public/client/less dirs for which files are included
-*/
-gulp.task('default',function(){
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var tap = require('gulp-tap');
+var sourcemaps = require('gulp-sourcemaps');
+var babelify = require('babelify');
+var browserifyCss = require('browserify-css');
+var _ = require("lodash");
+var path = require("path");
+var fs = require("fs");
+var DataUri = require('datauri');
+var less = require('gulp-less');
 
-    //build js changes 
-
-    buildReact();
-
-    gulp.watch(['public/client/js/react/src/**']).on('change',function(){
-        buildReact();
-    })
-
-    var bundle = watchify(browserify({ cache: {}, packageCache: {}, entries:['./public/client/js/main.js'], plugin: [watchify]}));
-    bundle.transform(babelify.configure({presets: ["env", "react"]})).transform(browserifyCss, {
-        global: true,
-        processRelativeUrl: function(relativeUrl) {
-            if (_.contains(['.jpg','.png','.gif', "ttf", "woff", "woff2", "svg"], path.extname(relativeUrl))) {
-                // Embed image and font data with data URI
-                var DataUri = require('datauri');
-                var dUri = new DataUri(relativeUrl);
-                return dUri.content;
-            }
-            return relativeUrl;
-        }
-    });
-    bundle.on('update',rebundle);
-
-    //live reload of compiled files
-    livereload.listen();
-    gulp.watch(['app/views/*','public/js/client.js','public/css/*']).on('change',livereload.changed);
-
-    //build less css changes*/
-    gulp.watch('public/client/less/**').on('change', function(){
-        return buildLess();
-    })
-
-    function buildReact(){
-      return  gulp.src("./public/client/js/react/src/**/*.js")
-        .pipe(
-            babel({presets: ["env", "react"]})
-            .transform(browserifyCss, {
-                global: true,
-                processRelativeUrl: function(relativeUrl) {
-                    if (_.contains(['.jpg','.png','.gif', "ttf", "woff", "woff2", "svg"], path.extname(relativeUrl))) {
-                        // Embed image and font data with data URI
-                        var DataUri = require('datauri');
-                        var dUri = new DataUri(relativeUrl);
-                        return dUri.content;
-                    }
-                    return relativeUrl;
+function transformChain(b) {
+  return b
+    .transform(
+        browserifyCss,
+        {
+            global: true,
+            processRelativeUrl: function(relativeUrl) {
+              if(fs.existsSync(relativeUrl)) {
+                if(_.includes(['.jpg', '.png', '.gif', "ttf", "woff", "woff2", "svg"], path.extname(relativeUrl))) {
+                    console.log("Inlining " + relativeUrl);
+                    // Embed image and font data with data URI
+                    var dUri = new DataUri(relativeUrl);
+                    return dUri.content;
                 }
-            })
-        )
-        .pipe(gulp.dest('./public/client/js/react/build'));
-    }
+              }
+              return relativeUrl;
+            }
+        }
+    )
+    .transform(babelify);
+}
 
-    function rebundle(){
-        return bundle.bundle()
-        .on('error',function(e){
-            gutil.log('Browserify Error:', e);
-        })
-        .pipe(source('client.js'))
-        .pipe(buffer())
+gulp.task('build', function () {
+
+  return gulp.src('./public/client/js/react/src/**/*.js', {read: false}) // no need of reading file because browserify does.
+    // transform file objects using gulp-tap plugin
+    .pipe(tap(function (file) {
+      gutil.log('bundling ' + file.path);
+      // replace file contents with browserify's bundle stream
+      file.contents = transformChain(browserify(file.path, {debug: true})).bundle();
+    }))
+    // transform streaming contents into buffer contents (because gulp-sourcemaps does not support streaming contents)
+    .pipe(buffer())
+    // load and init sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    // write sourcemaps
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/client/js/react/build'));
+
+});
+
+gulp.task('libs', function() {
+  // set up the browserify instance on a task basis
+  var b = transformChain(browserify({
+    entries: './public/client/libs.js',
+    debug: true,
+  }));
+
+  return b.bundle()
+    .pipe(source('libs.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
         .pipe(uglify())
-        .pipe(gulp.dest('./public/js'));
-    }
-
-    function buildLess(){
-        return gulp.src('./public/client/less/**/*.less')
-        .pipe(less({
-            paths: [ path.join(__dirname, 'less', 'includes') ]
-        }))
-        .pipe(gulp.dest('./public/css'));        
-    }
-
-    return rebundle();
-
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/js/'));
 });
 
-/*
-Task: libs
-builds file of global libs
-see  /public/client/libs file for which files are included
-*/
-gulp.task('libs', function(){
-     return gulp.src('./public/client/libs.js')
-    .pipe(gulpbrowserify({
-        insertGlobals: true
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest('./public/js'))
+gulp.task('mapper', function() {
+  // set up the browserify instance on a task basis
+  var b = transformChain(browserify({
+    entries: './public/client/idbmap.js',
+    debug: true,
+  }));
+
+  return b.bundle()
+    .pipe(source('idbmap.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/js/'));
 });
 
-/*
-Task: mapper
-builds globablized mapper module for standalone use
-see  /public/client/mapper
-*/
-gulp.task('mapper', function(){
-    return gulp.src('./public/client/idbmap.js')
-    .pipe(gulpbrowserify({
-        insertGlobals: true
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest('./public/js'))
-})
+gulp.task('client', function() {
+  // set up the browserify instance on a task basis
+  var b = transformChain(browserify({
+    entries: './public/client/js/main.js',
+    debug: true,
+  }));
 
-/*
-Task: buildLess  [THIS IS USED FOR RELEASE BUILD PROCESSES]
-build public/client/less files to public/css files
-*/
+  return b.bundle()
+    .pipe(source('client.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/js/'));
+});
+
 gulp.task('buildLess',function(){
     return gulp.src('./public/client/less/**/*.less')
     .pipe(less({
         paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', ["client", "mapper", "libs", "build", "buildLess"], function() {
 });
--- postinstall.sh
+++ postinstall.sh
@@ -10,20 +10,6 @@ if [ ! -d public/js ]; then
     mkdir public/js
 fi
 
-#compile jsx to build files for server side use of React
-babel public/client/js/react/src/ --out-dir public/client/js/react/build/ --blacklist strict && \
-#compile full client side file with jsx transforms for browser side client
-browserify -o public/js/client.js  public/client/js/main.js -g browserify-css -t [ babelify --presets "env" "react" ] && \
-#minify client side file
-uglifyjs -o public/js/client.js public/js/client.js && \
-
-#node_modules/uglify-js/bin/uglifyjs -o public/components/underscore/underscore-min.js public/components/underscore/underscore.js
-browserify -o public/js/libs.js  public/client/libs.js && \
-uglifyjs -o public/js/libs.js public/js/libs.js && \
-
-browserify -o public/js/idbmap.js  public/client/idbmap.js && \
-uglifyjs -o public/js/idbmap.js public/js/idbmap.js && \
-
-gulp buildLess && \
+gulp
 
 echo "+++++++++++++ END POSTINSTALL +++++++++++++"

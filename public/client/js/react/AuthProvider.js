c91505b68ce948c308e4127cfda9a72ed941a060
mlluciano <mlluciano1@icloud.com>
2024-02-05 12:01:44 -0500

    Add missing auth contxt init

---
+++ public/client/js/react/AuthProvider.js
@@ -0,0 +1,45 @@
+import React, {createContext, useEffect, useState} from 'react';
+
+export const AuthContext = createContext(null);
+export const AuthProvider = ({ children }) => {
+    // Initialize user from localstorage if available, otherwise init as null.
+    const [user, setUser] = useState(() => {
+        const savedUser = localStorage.getItem('user');
+        return savedUser ? JSON.parse(savedUser) : null;
+    });
+
+    // Check if user has a valid session on the server, if yes the server will respond with data about the user's session.
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch('/api/session');
+                if (response.ok) {
+                    const data = await response.json(); // Added await here
+                    if (data.user) {
+                        login(data.user);
+                    }
+                }
+            } catch (error) {
+                console.error('Error checking session:', error);
+            }
+        })();
+    }, []);
+
+    const login = (userData) => {
+        localStorage.setItem('user', JSON.stringify(userData));
+        setUser(userData);
+    };
+
+    const logout = () => {
+        console.log('firing logout!')
+        localStorage.removeItem('user');
+        setUser(null);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
